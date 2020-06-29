3db019c08d18325603d925ac8051bf5fa714c0c5
Paco <francisco.isenberg@gmail.com>
2020-06-29 16:41:36 -0700

    Can get stock info but need a database

--- numbers.py
+++ numbers.py
@@ -3,58 +3,63 @@ import json
 import datetime
 import pytz
 
+# returns dictionary with keys "base_url", "iex_secret", "stocks"
 def iex_environtment_selection(env):
     with open("config.json", 'r') as json_data_file:
         config = json.load(json_data_file)
 
     if env == "prod":
-        base_url = "https://cloud.iexapis.com/stable"
-        iex_secret = iex_secret = config["iex_token"]["production"]
-        stocks = ['AAXN', 'MRNA', 'AMZN', 'VOO', 'VTI']
+        environment = {
+            "base_url": "https://cloud.iexapis.com/stable",
+            "iex_secret": config["iex_token"]["production"],
+            "stocks": ['AAXN', 'MRNA', 'AMZN', 'VOO', 'VTI']
+        }
     else:
-        base_url = "https://sandbox.iexapis.com/stable"
-        iex_secret = config["iex_token"]["sandbox"]
-        stocks = ['AMZN', 'VOO']
+        environment = {
+            "base_url": "https://sandbox.iexapis.com/stable",
+            "iex_secret": config["iex_token"]["sandbox"],
+            "stocks": ['AMZN', 'VOO']
+        }
+    return(environment)
 
-    return(base_url, iex_secret, stocks)
-
+# returns datetime object with utc time from a time given in milliseconds
 def epoch_to_utc(epoch_milliseconds):
     return(datetime.datetime.utcfromtimestamp(epoch_milliseconds/1000).replace(tzinfo=pytz.UTC))
 
+# returns datetime object converted to nyc timezone
 def utc_to_nyc(utc_datetime):
     # print list of all common timezones
     # print("\n".join(pytz.common_timezones))
     return(utc_datetime.astimezone(pytz.timezone("America/New_York")))
 
-def stock_quote(stocks):
+# for getting current stock info based on stocks in environment
+def stock_quote(environment):
+    # Parameters for query string
+    params = {
+        "token": environment["iex_secret"]
+        # "symbols": environment["stocks"]
+    }
     stock_info = {}
 
     # TODO: https://iexcloud.io/docs/api/#batch-requests
-    for stock in stocks:
-        print("\nAPI call: ",base_url+endpoint_paths["price"].format(stock))
+    for stock in environment["stocks"]:
+        print("\nAPI call: ",environment["base_url"]+"/stock/{}/quote".format(stock))
         req = requests.get(
-            base_url+endpoint_paths["price"].format(stock),
+            environment["base_url"]+"/stock/{}/quote".format(stock),
             params
         )
         print(req.status_code)
         stock_info[stock] = req.json() #type == dictionary
         for key in stock_info[stock]:
             print(key,":",stock_info[stock][key])
+            if ("Time" in key and
+                key != "latestTime" and
+                stock_info[stock][key] != None):
+                market_time = utc_to_nyc(epoch_to_utc(stock_info[stock][key]))
+                print(key,":",market_time)
 
     return(stock_info)
 
-base_url, iex_secret, stocks = iex_environtment_selection("")
+environment = iex_environtment_selection("")
 
-# Parameters for query string
-params = {
-    "token": iex_secret
-    # "symbols": stocks"
-}
-
-# headers = {
-# }
-
-
-endpoint_paths = {
-    "price":"/stock/{}/quote"
-}
+stock_quote(environment)
